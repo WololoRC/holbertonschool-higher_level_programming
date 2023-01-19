d7025cffdfb2530f817be9546451885f9a2a0bb7
WololoRC <wololoRC@proton.me>
2023-01-19 19:36:11 -0300

    tsk2

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -10,8 +10,11 @@ import sys
 a_len = len(sys.argv) - 1
 if a_len < 0:
     a_len = 0
-
-print("{} arguments.".format(a_len))
+    print("{} arguments.".format(a_len))
+elif a_len == 1:
+    print("{} argument:".format(a_len))
+else:
+    print("{} arguments:".format(a_len))
 
 for cnt in range(1, a_len + 1):
     print("{}: {}".format(cnt, sys.argv[cnt]))
