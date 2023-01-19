b595e203d13e06d328a9270748f111b60b21452b
WololoRC <wololoRC@proton.me>
2023-01-19 19:25:43 -0300

    tsk2

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+    print the number and list the arguments
+"""
+import sys
+
+a_len = len(sys.argv) - 1
+if a_len < 0:
+    a_len = 0
+
+print("{} arguments".format(a_len))
+
+for cnt in range(1, a_len + 1):
+    print("{}: {}".format(cnt, sys.argv[cnt]))
