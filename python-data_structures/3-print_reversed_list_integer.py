86e8af3a028b611ac2ee8ecd561f9595f73890be
WololoRC <wololoRC@proton.me>
2023-01-23 12:07:20 -0300

    tsk3

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -4,7 +4,7 @@ def print_reversed_list_integer(my_list):
     prints all integers of a list, in reverse order.
     """
 
-    if (my_list == None):
+    if (my_list is None):
         return None
 
     my_list.reverse()
