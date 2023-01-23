4fc58d4debd5da3ba2866ac9bc2e737292ec568e
WololoRC <wololoRC@proton.me>
2023-01-23 13:05:28 -0300

    tsk5

---
+++ python-data_structures/5-no_c.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    """
+    removes all characters c and C from a string.
+    """
+
+    a_list = []
+    a_str = ""
+
+    for i in my_string:
+        if (i != 'c') and (i != 'C'):
+            a_list.append(i)
+            a_str += i
+
+    my_string = a_str
+
+    return my_string
