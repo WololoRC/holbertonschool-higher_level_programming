ba71a5c51c4cf34b4483a658af8fe1c554683a50
WololoRC <wololoRC@proton.me>
2023-01-24 15:29:04 -0300

    tsk8

---
+++ python-more_data_structures/8-simple_delete.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def simple_delete(a_dictionary, key):
+    """
+    Deletes a key in a dictionary.
+    """
+
+    if (key in a_dictionary):
+        a_dictionary.pop(key)
+
+    return a_dictionary
