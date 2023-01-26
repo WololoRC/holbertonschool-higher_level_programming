64846968e722b2a4f0b0d81eabe9d29ebb60719c
WololoRC <wololoRC@proton.me>
2023-01-26 14:58:04 -0300

    Readme

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -7,6 +7,8 @@ def safe_print_list(my_list, x):
             cnt += 1
             if (cnt >= x):
                 break
+    except:
+       print("Wololo") 
     finally:
         print()
         return cnt
