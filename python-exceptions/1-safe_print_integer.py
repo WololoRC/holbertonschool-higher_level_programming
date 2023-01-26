6f29a8eea1d614fe874fc1fb0444f29a87e757e3
WololoRC <wololoRC@proton.me>
2023-01-26 16:31:59 -0300

    tsk1

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -7,7 +7,6 @@ def safe_print_integer(value):
     try:
         print("{:d}".format(value))
         return True
-    except ValueError:
-        return False
-    else:
+
+    except TypeError:
         return False
