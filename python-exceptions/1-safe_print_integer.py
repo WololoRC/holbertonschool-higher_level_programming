deb94c10684ed1d3a4e794d2012b8c409110fe73
WololoRC <wololoRC@proton.me>
2023-01-26 16:28:35 -0300

    tsk1

---
+++ python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_integer(value):
+    """
+    Print value and return True if is Integer, else return False
+    """
+
+    try:
+        print("{:d}".format(value))
+        return True
+    except ValueError:
+        return False
