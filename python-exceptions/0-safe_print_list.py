bff8b97cbcc143c56d3f788779e0178a92c83c4a
WololoRC <wololoRC@proton.me>
2023-01-26 16:20:30 -0300

    not sure

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -3,12 +3,12 @@ def safe_print_list(my_list, x):
     cnt = 0
     try:
         for i in my_list:
+            if ((cnt >= x) or (x == 0)):
+                break
             print("{}".format(i), end="")
             cnt += 1
-            if (cnt >= x):
-                break
-    except:
-       print("Wololo") 
+    except NameError:
+        print("{:d}".format(my_list))
     finally:
         print()
         return cnt
