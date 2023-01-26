cbbd611a4c20db2abcf54d9528aeebc454ef7a1f
WololoRC <wololoRC@proton.me>
2023-01-26 10:12:06 -0300

    tsk12

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if (roman_string):
+    if (roman_string and type(roman_string) is str):
         latin = {'I': 1, 'V': 5, 'X': 10, 'C': 100, 'L': 50, 'D': 500}
-        arabic = []
-        res = 0
-
-        for nums in roman_string:
-            if nums in latin:
-                arabic.append(latin.get(nums))
-
-        for i in arabic:
-            res += i
+        res, crt, cnt = 0, 0, 0
+        for nums in reversed(roman_string):
+            if nums in roman_string:
+                if crt > latin[nums]:
+                    res -= latin[nums]
+                else:
+                    res += latin[nums]
+                crt = latin[nums]
 
         return res
 
