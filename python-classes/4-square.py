48b2bcb5a11ad8684e8d6b0aae22e0ff30a1f0e0
WololoRC <wololoRC@proton.me>
2023-01-31 17:06:25 -0300

    tsk4

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -15,34 +15,8 @@ class Square:
         the size of the square
 
     """
-    __size = 0
     def __init__(self, size=0):
         """
-        Parameters
-        ----------
-        size : int, optional
-            the size of the square (default 0)
-
-        Raises
-        ------
-        TypeError
-            if size is not int
-        ValueError.
-            if size is < 0
-        """
-
-        if type(size) == int:
-            self.__size = size
-        else:
-            raise TypeError("size must be an integer")
-        if (size < 0):
-            raise ValueError("size must be >= 0")
-
-
-    @Square.self.__size.setter
-    def size(self, value):
-        """Setts new size
-
         Parameters
         ----------
         size : int, optional
@@ -69,6 +43,29 @@ class Square:
 
         return self.__size
 
+    @size.setter
+    def size(self, value):
+        """Setts new size
+
+        Parameters
+        ----------
+        size : int, optional
+            the size of the square (default 0)
+
+        Raises
+        ------
+        TypeError
+            if size is not int
+        ValueError.
+            if size is < 0
+        """
+
+        if type(value) == int:
+            self.__size = value
+        else:
+            raise TypeError("size must be an integer")
+        if (value < 0):
+            raise ValueError("size must be >= 0")
 
     def area(self):
         """Returns the area of the square"""
