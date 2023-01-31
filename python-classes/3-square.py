b2ebb75f31bbdcecb4b3f615d274ff68f2fb4fbc
WololoRC <wololoRC@proton.me>
2023-01-31 15:36:27 -0300

    tsk3

---
+++ python-classes/3-square.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""
+Defines a class called Square
+
+"""
+
+
+class Square:
+    """
+    A Square
+
+    Attributes
+    ---------
+    size : int
+        the size of the square
+
+    """
+    def __init__(self, size=0):
+        """
+        Parameters
+        ----------
+        size : int, optional
+            the size of the square (default 0)
+
+        Raises
+        ------
+        TypeError
+            if size is not int
+        ValueError.
+            if size is < 0
+        """
+
+        if type(size) == int:
+            self.__size = size
+        else:
+            raise TypeError("size must be an integer")
+        if (size < 0):
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        """Returns the area of the square"""
+
+        return self.__size ** 2
