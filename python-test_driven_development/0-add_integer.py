1db3a48dc279dd6068e2e3d1f5018826819b1132
WololoRC <wololoRC@proton.me>
2023-02-01 17:35:57 -0300

    tsk0

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""0-add_integer module, supplies add_integer function"""
+
+
+def add_integer(a, b=98):
+    """add two numbers.
+
+    Args
+    ----
+    a : int or floar
+    b : int or float
+
+    Return
+    ------
+    a + b : int
+    """
+
+    if ((type(a) != int) and (type(a) != float)):
+        raise TypeError("a must be an integer")
+
+    if ((type(b) != int) and (type(b) != float)):
+        raise TypeError("b must be an integer")
+
+    return (int(a) + int(b))
