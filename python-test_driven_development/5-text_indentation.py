331b03eb080421f3ca18951f342e9b0497331569
WololoRC <wololoRC@proton.me>
2023-02-03 14:34:18 -0300

    tsk5

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""5-text_indentation module: holds text_indetation func"""
+
+
+def text_indentation(text):
+    """
+    indent a text with two "\n" after: ".",":","?"'s
+
+    Properties
+    ----------
+    text : str
+        A string to indent and print
+
+    Raises
+    ------
+    TypeError
+        if @text != string
+    """
+    if type(text) is str:
+        x = text.replace(". ", ".\n\n")
+        x = x.replace("? ", "?\n\n")
+        x = x.replace(": ", ":\n\n")
+        print(x)
+    else:
+        raise TypeError("text must be a string")
