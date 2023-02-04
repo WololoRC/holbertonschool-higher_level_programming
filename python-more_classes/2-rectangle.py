7e3d5cd8507e92134d046ea257ba2db95d1be87d
WololoRC <wololoRC@proton.me>
2023-02-04 02:39:24 -0300

    tsk2

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -36,6 +36,9 @@ class Rectangle:
 
     def perimeter(self):
         """returns the perimeter of a Rectangle instance"""
+        if self.__height == 0 or self.__width == 0:
+            return 0
+
         return (2 * (self.__height + self.__width))
 
     @property
