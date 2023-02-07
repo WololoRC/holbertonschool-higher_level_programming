7d13d67fff957876952193bf3ad32cee38df0baa
WololoRC <wololoRC@proton.me>
2023-02-07 19:50:08 -0300

    tsk2

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -4,7 +4,6 @@
 
 def is_same_class(obj, a_class):
     """return True if @obj is instance of @a_class"""
-    if isinstance(obj, a_class):
-        return True
-    else:
-        return False
+    print(f"obj: {obj}")
+    print(f"a_class: [a_class]")
+    return issubclass(a_class, type(obj))
