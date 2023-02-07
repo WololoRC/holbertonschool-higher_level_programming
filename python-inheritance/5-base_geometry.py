5ccb5136ab8fe38085e73254e1d221e054c64c7f
WololoRC <wololoRC@proton.me>
2023-02-07 20:55:52 -0300

    tsk6

--- python-inheritance/5-base_geometry.py
+++ python-inheritance/5-base_geometry.py
@@ -3,4 +3,5 @@
 
 
 class BaseGeometry():
+    """betwen no more and drink a seat"""
     pass
--- python-inheritance/6-base_geometry.py
+++ python-inheritance/6-base_geometry.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Holads BaseGeometry class"""
+"""Holad BaseGeometry class"""
 
 
 class BaseGeometry():
