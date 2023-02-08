ff71b2e0c43c1228c627cd1efbc3be0fdb8f9f58
WololoRC <wololoRC@proton.me>
2023-02-08 16:33:40 -0300

    tsk1

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -3,6 +3,8 @@
 
 
 def write_file(filename="", text=""):
-    """write on a file and return num og chars writen"""
+    """write on a file and return num of chars writen
+                    on current firectory
+    """
     with open(filename, "w", encoding="utf-8") as a_file:
         return a_file.write(text)
