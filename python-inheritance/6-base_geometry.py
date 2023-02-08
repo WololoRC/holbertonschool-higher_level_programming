4f3efb0577bb2096082a47e4dd635a04c1189488
WololoRC <wololoRC@proton.me>
2023-02-07 23:18:06 -0300

    tsk6

--- python-inheritance/6-base_geometry.py
+++ python-inheritance/6-base_geometry.py
@@ -4,19 +4,6 @@
 
 class BaseGeometry():
     """Documentation in progress"""
-
     def area(self):
         """Documentation in progress"""
         return Exception("area() is not implemented")
-
-
-    def integer_validator(self, name, value):
-        """validates integer"""
-
-        if type(value) is int:
-            if value > 0:
-                return value
-            else:
-                raise ValueError(f"{name} must be greater than 0")
-        else:
-            raise TypeError(f"{name} must be an integer")
