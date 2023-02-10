f4f99d5d228c29dd680f36e0f8f4745d0dde4c51
WololoRC <wololoRC@proton.me>
2023-02-10 18:05:51 -0300

    tsk10

---
+++ python-input_output/10-student.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""Hols: student class"""
+
+
+class Student():
+    """
+    A Student
+
+    Atributes
+    ---------
+    first_name : str
+        his name
+
+    last_name : str
+        his last name
+
+    age : int
+        his age
+
+    Methods
+    -------
+    to_json:
+        see below...
+    """
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Return __dict__ for json pruposites"""
+
+        if attrs is None:
+            return self.__dict__
+
+        else:
+            return {k: v for k, v in self.__dict__.items() if k in attrs}
