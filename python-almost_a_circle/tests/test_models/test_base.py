e1637e0e3786f23a92fea01ea8039c3c48ed72b4
WololoRC <wololoRC@proton.me>
2023-02-17 17:26:24 -0300

    end on baste tests

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -15,7 +15,7 @@ class Test_classBase(unittest.TestCase):
     def test_id(self):
         """id without declarations"""
         instance1 = Base()
-        self.assertEqual(instance1.id, 2)
+        self.assertEqual(instance1.id, 3)
 
     def test_passing_id(self):
         """id with declaration"""
@@ -25,9 +25,10 @@ class Test_classBase(unittest.TestCase):
     def test_new_instance(self):
         """new instance how many id's?"""
         instance2 = Base()
-        self.assertEqual(instance2.id, 4)
+        self.assertEqual(instance2.id, 5)
 
     def test_base_to_json_str(self):
+        """tests on base to json stirng"""
         x = Base()
 
         """None == []"""
@@ -51,6 +52,22 @@ class Test_classBase(unittest.TestCase):
         a_dict = {'id': 12}
         return_test = x.to_json_string([a_dict])
         self.assertTrue(return_test == '[{"id": 12}]')
+
+    def test_from_json_string(self):
+        """tests on from_json_string"""
+
+        """None == [] ?"""
+        x = Base()
+
+        return_test = x.from_json_string(None)
+        self.assertEqual(return_test, [])
+
+        """Is the dict?"""
+        return_test = x.from_json_string('[{ "id": 89 }]')
+        self.assertTrue(return_test == [{ "id": 89 }])
+
+        """is the list?"""
+        self.assertTrue(type(return_test) is list)
 
 if __name__ == "__main__":
     unittest.main()
