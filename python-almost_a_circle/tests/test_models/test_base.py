0a1bdaae42cfd12992c812e6ce9c72e0b41399a5
WololoRC <wololoRC@proton.me>
2023-02-17 16:48:07 -0300

    see checker on tests

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -15,7 +15,7 @@ class Test_classBase(unittest.TestCase):
     def test_id(self):
         """id without declarations"""
         instance1 = Base()
-        self.assertEqual(instance1.id, 1)
+        self.assertEqual(instance1.id, 2)
 
     def test_passing_id(self):
         """id with declaration"""
@@ -23,9 +23,34 @@ class Test_classBase(unittest.TestCase):
         self.assertEqual(instance1.id, 5)
 
     def test_new_instance(self):
-        """new instance + 1 id?"""
+        """new instance how many id's?"""
         instance2 = Base()
-        self.assertEqual(instance2.id, 3)
+        self.assertEqual(instance2.id, 4)
+
+    def test_base_to_json_str(self):
+        x = Base()
+
+        """None == []"""
+        return_test = x.to_json_string(None)
+        self.assertEqual(return_test, "[]")
+
+        """empty dic == []"""
+        return_test = x.to_json_string({})
+        self.assertEqual(return_test, "[]")
+
+        """empty list"""
+        return_test = x.to_json_string([])
+        self.assertEqual(return_test, "[]")
+
+        """it works!"""
+        a_dict = {'id': 12}
+        return_test = x.to_json_string([a_dict])
+        self.assertTrue(isinstance(return_test, str))
+
+        """id == 12"""
+        a_dict = {'id': 12}
+        return_test = x.to_json_string([a_dict])
+        self.assertTrue(return_test == '[{"id": 12}]')
 
 if __name__ == "__main__":
     unittest.main()
---
+++ python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Tesr on Rectangle Class"""
+import unittest
+from models.rectangle import Rectangle
+
+
+class Test_classRectangle(unittest.TestCase):
+    def test_Rectangle_attributes(self):
+        """Attrbiutes fine?"""
+
+        """width and height"""
+        instance1 = Rectangle(1, 2)
+        self.assertEqual(instance1.width, 1)
+        self.assertEqual(instance1.height, 2)
+
+        """width, height and x"""
+        instance2 = Rectangle(1, 2, 3)
+        self.assertEqual(instance2.width, 1)
+        self.assertEqual(instance2.height, 2)
+        self.assertEqual(instance2.x, 3)
+
+        """width, height, x and y"""
+        instance2 = Rectangle(1, 2, 3, 4)
+        self.assertEqual(instance2.width, 1)
+        self.assertEqual(instance2.height, 2)
+        self.assertEqual(instance2.x, 3)
+        self.assertEqual(instance2.y, 4)
