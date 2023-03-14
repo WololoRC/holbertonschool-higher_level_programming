e28c8c033b7c86c94bd280f27e76a35335bdd577
WololoRC <wololoRC@proton.me>
2023-03-14 17:48:11 -0300

    fix

--- SQL_introduction/16-no_link.sql
+++ SQL_introduction/16-no_link.sql
@@ -1,2 +1,2 @@
 -- List all record of @second_table by desc order
-SELECT score, name FROM second_table ORDER BY score DESC;
+SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;
