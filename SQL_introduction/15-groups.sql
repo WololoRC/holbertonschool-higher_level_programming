cb5f786ec02d1440157eb1a8591d2eb24449917d
WololoRC <wololoRC@proton.me>
2023-03-14 17:36:21 -0300

    tsk15

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -1,2 +1,2 @@
 -- lists number of recors with the same scote on @second_table
-SELECT score, COUNT(score) AS number FROM second_table GROUP BY score;
+SELECT score, COUNT(score) AS number FROM second_table GROUP BY score ORDER BY score DESC;
