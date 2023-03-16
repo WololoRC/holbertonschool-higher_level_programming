6b56a048fe10cad1cce71f37d507af6279c451a3
WololoRC <wololoRC@proton.me>
2023-03-16 17:06:08 -0300

    tsk9 fix

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,2 +1,2 @@
 -- List all cities by citie.id, cite.name, state.name.
-SELECT cities.id, cities.name, states.name FROM states, cities WHERE citie.state_id=state.id;
+SELECT cities.id, cities.name, states.name FROM states, cities WHERE cities.state_id=states.id;
