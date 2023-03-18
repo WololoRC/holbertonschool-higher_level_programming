a4816e308618ba4fcc090766ba48a37407fdbb88
WololoRC <wololoRC@proton.me>
2023-03-18 16:08:20 -0300

    tsk8

---
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" Create a Session instance and made and list by states.id=1 query
+"""
+
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+from sys import argv
+from model_state import Base, State
+
+if __name__ == "__main__":
+    engine = create_engine(
+            'mysql+mysqldb://{}:{}@localhost/{}'
+            .format(argv[1], argv[2], argv[3]), pool_pre_ping=True)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    for record in session.query(State).filter(State.id==1):
+        print(f"{record.id}: {record.name}")
