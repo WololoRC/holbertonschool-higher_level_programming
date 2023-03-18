afd4ee2cd2b561650d127bde8d1fb3a995513082
WololoRC <wololoRC@proton.me>
2023-03-18 03:08:51 -0300

    tsk4

---
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Lists record with on @hbtn_0e_4_usa DB"""
+import MySQLdb
+from sys import argv
+
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+            host='localhost', user=argv[1], passwd=argv[2], db=argv[3])
+
+    cur = db.cursor()
+
+    cur.execute("SELECT cities.id, cities.name, states.name FROM cities LEFT JOIN states ON states.id=cities.state_id")
+    rows = (cur.fetchall())
+    for row in rows:
+        print(row)
