07ae82074ed2d31556624a1dd65f70a09a9a2414
WololoRC <wololoRC@proton.me>
2023-03-18 02:09:59 -0300

    tsk1 case sensitive

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
             host='localhost', user=argv[1], passwd=argv[2], db=argv[3])
 
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%'")
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%'")
     rows = (cur.fetchall())
     for row in rows:
         print(row)
