ee5377825da0897a04a82947b0026bf21e845fda
WololoRC <wololoRC@proton.me>
2023-03-18 15:56:55 -0300

    whats wrong?

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -7,13 +7,13 @@ from sqlalchemy import create_engine
 from sys import argv
 from model_state import Base, State
 
-engine = create_engine(
-        'mysql+mysqldb://{}:{}@localhost/{}'
-        .format(argv[1], argv[2], argv[3]), pool_pre_ping=True)
+if __name__ == "__main__":
+    engine = create_engine(
+            'mysql+mysqldb://{}:{}@localhost/{}'
+            .format(argv[1], argv[2], argv[3]), pool_pre_ping=True)
 
-Session = sessionmaker(bind=engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
-session = Session()
-
-for record in session.query(State).order_by(State.id):
-    print(f"{record.id}: {record.name}")
+    for record in session.query(State).order_by(State.id):
+        print(f"{record.id}: {record.name}")
