24a9d0d51258daeab1934ed7c89525b6b3c05e0e
WololoRC <wololoRC@proton.me>
2023-03-18 02:13:59 -0300

    tsk2

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Lists record with script arguments"""
+import MySQLdb
+from sys import argv
+
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+            host='localhost', user=argv[1], passwd=argv[2], db=argv[3])
+
+    cur = db.cursor()
+    cur.execute(f"SELECT * FROM states WHERE name='{argv[4]}'")
+    rows = (cur.fetchall())
+    for row in rows:
+        print(row)
