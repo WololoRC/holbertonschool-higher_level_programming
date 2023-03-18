63302275beef723efbda41e603afbead0cf9c68f
WololoRC <wololoRC@proton.me>
2023-03-18 01:28:55 -0300

    tsk0

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""This is my first Python+MySQL script"""
+import MySQLdb
+from sys import argv
+
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+            host='localhost', user=argv[1], passwd=argv[2], db=argv[3])
+
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states")
+    rows = (cur.fetchall())
+    for row in rows:
+        print(row)
