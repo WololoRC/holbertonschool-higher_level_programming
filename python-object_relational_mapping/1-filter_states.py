22a813e4e61ab21efd0594e65ad833818583c3ed
WololoRC <wololoRC@proton.me>
2023-03-18 01:55:22 -0300

    tsk1

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Lists all recors how his name starts with the leltter N"""
+import MySQLdb
+from sys import argv
+
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+            host='localhost', user=argv[1], passwd=argv[2], db=argv[3])
+
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%'")
+    rows = (cur.fetchall())
+    for row in rows:
+        print(row)
