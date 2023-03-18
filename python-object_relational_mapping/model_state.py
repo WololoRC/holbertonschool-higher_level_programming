5a68cde55fb5cf865924a19a0774d398f3f19cc4
WololoRC <wololoRC@proton.me>
2023-03-18 15:20:11 -0300

    tsk6

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -13,6 +13,7 @@ from sqlalchemy.ext.declarative import declarative_base
 Base = declarative_base()
 
 class State(Base):
+    """mapping class for record on @state table"""
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)
