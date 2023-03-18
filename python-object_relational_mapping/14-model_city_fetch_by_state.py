8507f0e3d1f790b4509cbecd9204c49400b10ced
WololoRC <wololoRC@proton.me>
2023-03-18 19:01:39 -0300

    tsk14

---
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Join and lists all records on City and State
+"""
+
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+from sys import argv
+from model_state import Base, State
+from model_city import City
+
+
+if __name__ == "__main__":
+    engine = create_engine(
+            'mysql+mysqldb://{}:{}@localhost/{}'
+            .format(argv[1], argv[2], argv[3]), pool_pre_ping=True)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    for a, b in session.query(
+            State, City).filter(State.id == City.state_id).all():
+
+        print(f"{a.name}: {b.id} {b.name}")
