86c5e2afcd54e9505efbcc79ebb066885722eac5
WololoRC <wololoRC@proton.me>
2023-03-18 02:41:02 -0300

    tsk3

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Lists record with script arguments"""
+import MySQLdb
+from sys import argv
+
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+            host='localhost', user=argv[1], passwd=argv[2], db=argv[3])
+
+    cur = db.cursor()
+
+    if (len(argv[4]) <= 10):
+        cur.execute("SELECT * FROM states WHERE BINARY name='{}'".format(argv[4]))
+        rows = (cur.fetchall())
+        for row in rows:
+            print(row)
+
+    else:
+        print('nope')
