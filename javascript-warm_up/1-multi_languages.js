b2bb87910be663f04ebd1739a536b21cddd3a0fa
WololoRC <wololoRC@proton.me>
2023-04-15 15:29:31 -0300

    task 1 - 2

---
+++ javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+const MyArray = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+const len = MyArray.length;
+
+for (let cnt = 0; cnt < len; cnt++) {
+  console.log(MyArray[cnt]);
+}
