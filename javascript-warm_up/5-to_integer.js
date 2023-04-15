e6ec946c32f0a8090014f0828c0c7566c93e12fd
WololoRC <wololoRC@proton.me>
2023-04-15 17:32:08 -0300

    task 5

---
+++ javascript-warm_up/5-to_integer.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// Turn a String into Number
+const args = process.argv.slice(2);
+const toNumber = parseInt(args[0]);
+
+if (toNumber) {
+  console.log('My number:', toNumber);
+} else {
+  console.log('Not a number');
+}
