f5d1e278331d0ddd083854c19a523b6732f9918a
WololoRC <wololoRC@proton.me>
2023-04-17 16:21:15 -0300

    tsk3

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -3,7 +3,7 @@
 const request = require('request');
 const args = process.argv.slice(2);
 
-request('https://swapi.dev/api/films/' + args[0] + '/', function (error, response, body) {
+request('https://swapi-api.alx-tools.com/api/films/' + args[0] + '/', function (error, response, body) {
   if (error) {
     console.error('error:', error);
   }
